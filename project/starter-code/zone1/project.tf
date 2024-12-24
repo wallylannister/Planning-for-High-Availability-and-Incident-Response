60eff19064ded7062825d95abef23b9d6dfb2d71
wallylannister <tuongvo2808@gmail.com>
2024-12-24 14:00:36 +0700

    2nd

--- project/starter-code/zone1/project.tf
+++ project/starter-code/zone1/project.tf
@@ -7,29 +7,4 @@ resource "kubernetes_namespace" "udacity" {
    ]
  }
 
-  resource "kubernetes_service" "grafana-external" {
-  metadata {
-    name      = "grafana-external"
-    namespace = "monitoring"
-    annotations = {
-      "service.beta.kubernetes.io/aws-load-balancer-type"            = "nlb"
-      "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
-    }
-  }
-  spec {
-    selector = {
-      "app.kubernetes.io/name"="grafana"
-    }
-
-    port {
-      port        = 80
-      target_port = 3000
-    }
-
-    type = "LoadBalancer"
-  }
-
-  depends_on = [
-    module.project_eks
-  ]
-}
+  
